631d7bb8973b95227064ff59d8b29ced563e6e74
Erruar <105001030+Erruar@users.noreply.github.com>
2024-07-19 09:08:02 +0300

    11.22

--- Saku Overclock.Core/obj/Debug/net7.0/Saku Overclock.Core.AssemblyInfo.cs
+++ Saku Overclock.Core/obj/Debug/net7.0/Saku Overclock.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Saku Overclock.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.1.5")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfd05017962a936d03e0db04a7190a8a7188239e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63c1417577216c10dc2f68a2897df38eb0453dc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Saku Overclock.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Saku Overclock.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.1.5")]
