a04acb4d2fe4fb854911495244fefc28f32b3ce0
Erruar <erruarbrorder@gmail.com>
2024-12-19 22:05:07 +0300

    Cleaned useless variables on SMUAdressSet backend

--- Saku Overclock/SMUEngine/SMUAdressSet.cs
+++ Saku Overclock/SMUEngine/SMUAdressSet.cs
@@ -2,23 +2,9 @@
 /*This is a modified processor driver file. Zen-States.Core Version is 1.6.8.1. Its author is https://github.com/irusanov
 This file has been refactored many times and optimized to work with Saku Overclock by Sakurazhima Serzhik. I do not recommend rereading this file, it is better to familiarize yourself with https://github.com/irusanov/ZenStates-Core
 there you can see the source files in detail*/
-public class SmuAddressSet
+public class SmuAddressSet(uint msgAddress, uint rspAddress, uint argAddress)
 {
-    public uint MsgAddress;
-    public uint RspAddress;
-    public uint ArgAddress;
-
-    public SmuAddressSet()
-    {
-        MsgAddress = 0;
-        RspAddress = 0;
-        ArgAddress = 0;
-    }
-
-    public SmuAddressSet(uint msgAddress, uint rspAddress, uint argAddress)
-    {
-        MsgAddress = msgAddress;
-        RspAddress = rspAddress;
-        ArgAddress = argAddress;
-    }
+    public readonly uint MsgAddress = msgAddress;
+    public readonly uint RspAddress = rspAddress;
+    public readonly uint ArgAddress = argAddress;
 }
