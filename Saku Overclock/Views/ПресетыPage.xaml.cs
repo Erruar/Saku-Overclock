49e0de34006ace30495cee687f3796e786d09977
Erruar <erruarbrorder@gmail.com>
2024-12-22 20:37:06 +0300

    Updated ПресетыPage backend for new app config

--- Saku Overclock/Views/ПресетыPage.xaml.cs
+++ Saku Overclock/Views/ПресетыPage.xaml.cs
@@ -1,33 +1,32 @@
 ﻿using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Media.Imaging;
-using Newtonsoft.Json;
-using Saku_Overclock.Helpers;
+using Microsoft.UI.Xaml.Media.Imaging; 
+using Saku_Overclock.Contracts.Services;
+using Saku_Overclock.Helpers; 
 using Saku_Overclock.ViewModels;
 
 namespace Saku_Overclock.Views;
 
 public sealed partial class ПресетыPage
-{
-    private Config _config = new();
+{ 
     private bool _relay;
+    private static readonly IAppSettingsService SettingsService = App.GetService<IAppSettingsService>();
+
 
     public ПресетыPage()
     {
         App.GetService<ПресетыViewModel>();
-        InitializeComponent();
-        ConfigLoad();
+        InitializeComponent(); 
         InitSave();
-        _config.NBFCFlagConsoleCheckSpeedRunning = false;
-        _config.FlagRyzenADJConsoleTemperatureCheckRunning = false;
-        ConfigSave();
+        SettingsService.NBFCFlagConsoleCheckSpeedRunning = false;
+        SettingsService.FlagRyzenADJConsoleTemperatureCheckRunning = false;
+        SettingsService.SaveSettings();
     }
 
     #region JSON and Initialization
 
     private void InitSave()
-    {
-        ConfigLoad();
-        if (_config.PremadeMaxActivated)
+    { 
+        if (SettingsService.PremadeMaxActivated)
         {
             Max_btn.IsChecked = true;
             PrSource.ImageSource = new BitmapImage(new Uri("ms-appx:///Assets/max.png"));
@@ -35,7 +34,7 @@ public sealed partial class ПресетыPage
             PrDesc.Text = "Preset_Max_Desc".GetLocalized();
         }
 
-        if (_config.PremadeSpeedActivated)
+        if (SettingsService.PremadeSpeedActivated)
         {
             Speed.IsChecked = true;
             PrSource.ImageSource = new BitmapImage(new Uri("ms-appx:///Assets/speed.png"));
@@ -43,7 +42,7 @@ public sealed partial class ПресетыPage
             PrDesc.Text = "Preset_Speed_Desc".GetLocalized();
         }
 
-        if (_config.PremadeBalanceActivated)
+        if (SettingsService.PremadeBalanceActivated)
         {
             Balance.IsChecked = true;
             PrSource.ImageSource = new BitmapImage(new Uri("ms-appx:///Assets/balance.png"));
@@ -51,7 +50,7 @@ public sealed partial class ПресетыPage
             PrDesc.Text = "Preset_Balance_Desc".GetLocalized();
         }
 
-        if (_config.PremadeEcoActivated)
+        if (SettingsService.PremadeEcoActivated)
         {
             Eco.IsChecked = true;
             PrSource.ImageSource = new BitmapImage(new Uri("ms-appx:///Assets/eco.png"));
@@ -59,44 +58,14 @@ public sealed partial class ПресетыPage
             PrDesc.Text = "Preset_Eco_Desc".GetLocalized();
         }
 
-        if (_config.PremadeMinActivated)
+        if (SettingsService.PremadeMinActivated)
         {
             Min_btn.IsChecked = true;
             PrSource.ImageSource = new BitmapImage(new Uri("ms-appx:///Assets/min.png"));
             PrName.Text = "Preset_Min".GetLocalized();
             PrDesc.Text = "Preset_Min_Desc".GetLocalized();
         }
-    }
-
-    private void ConfigSave()
-    {
-        try
-        {
-            Directory.CreateDirectory(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal),
-                "SakuOverclock"));
-            File.WriteAllText(
-                Environment.GetFolderPath(Environment.SpecialFolder.Personal) + @"\SakuOverclock\config.json",
-                JsonConvert.SerializeObject(_config));
-        }
-        catch
-        {
-            // ignored
-        }
-    }
-
-    private void ConfigLoad()
-    {
-        try
-        {
-            _config = JsonConvert.DeserializeObject<Config>(File.ReadAllText(
-                Environment.GetFolderPath(Environment.SpecialFolder.Personal) + @"\SakuOverclock\config.json"))!;
-        }
-        catch
-        {
-            _config = new Config();
-        }
-    }
-
+    } 
     #endregion
 
     #region Event Handlers
@@ -107,18 +76,17 @@ public sealed partial class ПресетыPage
         Eco.IsChecked = false;
         Balance.IsChecked = false;
         Speed.IsChecked = false;
-        Max_btn.IsChecked = false;
-        ConfigLoad();
-        _config.PremadeMinActivated = true;
-        _config.PremadeEcoActivated = false;
-        _config.PremadeBalanceActivated = false;
-        _config.PremadeSpeedActivated = false;
-        _config.PremadeMaxActivated = false;
-        _config.RyzenADJline =
+        Max_btn.IsChecked = false; 
+        SettingsService.PremadeMinActivated = true;
+        SettingsService.PremadeEcoActivated = false;
+        SettingsService.PremadeBalanceActivated = false;
+        SettingsService.PremadeSpeedActivated = false;
+        SettingsService.PremadeMaxActivated = false;
+        SettingsService.RyzenADJline =
             " --tctl-temp=60 --stapm-limit=9000 --fast-limit=9000 --stapm-time=900 --slow-limit=6000 --slow-time=900 --vrm-current=120000 --vrmmax-current=120000 --vrmsoc-current=120000 --vrmsocmax-current=120000 --vrmgfx-current=120000 --prochot-deassertion-ramp=2 ";
-        ConfigSave();
+        SettingsService.SaveSettings();
         InitSave();
-        MainWindow.Applyer.Apply(_config.RyzenADJline, false, _config.ReapplyOverclock, _config.ReapplyOverclockTimer);
+        MainWindow.Applyer.Apply(SettingsService.RyzenADJline, false, SettingsService.ReapplyOverclock, SettingsService.ReapplyOverclockTimer);
     }
 
     private void Eco_Checked(object sender, RoutedEventArgs e)
@@ -127,18 +95,17 @@ public sealed partial class ПресетыPage
         Min_btn.IsChecked = false;
         Balance.IsChecked = false;
         Speed.IsChecked = false;
-        Max_btn.IsChecked = false;
-        ConfigLoad();
-        _config.PremadeMinActivated = false;
-        _config.PremadeEcoActivated = true;
-        _config.PremadeBalanceActivated = false;
-        _config.PremadeSpeedActivated = false;
-        _config.PremadeMaxActivated = false;
-        _config.RyzenADJline =
+        Max_btn.IsChecked = false; 
+        SettingsService.PremadeMinActivated = false;
+        SettingsService.PremadeEcoActivated = true;
+        SettingsService.PremadeBalanceActivated = false;
+        SettingsService.PremadeSpeedActivated = false;
+        SettingsService.PremadeMaxActivated = false;
+        SettingsService.RyzenADJline =
             " --tctl-temp=68 --stapm-limit=15000  --fast-limit=18000 --stapm-time=500 --slow-limit=16000 --slow-time=500 --vrm-current=120000 --vrmmax-current=120000 --vrmsoc-current=120000 --vrmsocmax-current=120000 --vrmgfx-current=120000 --prochot-deassertion-ramp=2 ";
-        ConfigSave();
+        SettingsService.SaveSettings();
         InitSave();
-        MainWindow.Applyer.Apply(_config.RyzenADJline, false, _config.ReapplyOverclock, _config.ReapplyOverclockTimer);
+        MainWindow.Applyer.Apply(SettingsService.RyzenADJline, false, SettingsService.ReapplyOverclock, SettingsService.ReapplyOverclockTimer);
     }
 
     private void Balance_Checked(object sender, RoutedEventArgs e)
@@ -147,18 +114,17 @@ public sealed partial class ПресетыPage
         Eco.IsChecked = false;
         Min_btn.IsChecked = false;
         Speed.IsChecked = false;
-        Max_btn.IsChecked = false;
-        ConfigLoad();
-        _config.PremadeMinActivated = false;
-        _config.PremadeEcoActivated = false;
-        _config.PremadeBalanceActivated = true;
-        _config.PremadeSpeedActivated = false;
-        _config.PremadeMaxActivated = false;
-        _config.RyzenADJline =
+        Max_btn.IsChecked = false; 
+        SettingsService.PremadeMinActivated = false;
+        SettingsService.PremadeEcoActivated = false;
+        SettingsService.PremadeBalanceActivated = true;
+        SettingsService.PremadeSpeedActivated = false;
+        SettingsService.PremadeMaxActivated = false;
+        SettingsService.RyzenADJline =
             " --tctl-temp=75 --stapm-limit=17000  --fast-limit=20000 --stapm-time=64 --slow-limit=19000 --slow-time=128 --vrm-current=120000 --vrmmax-current=120000 --vrmsoc-current=120000 --vrmsocmax-current=120000 --vrmgfx-current=120000 --prochot-deassertion-ramp=2";
-        ConfigSave();
+        SettingsService.SaveSettings();
         InitSave();
-        MainWindow.Applyer.Apply(_config.RyzenADJline, false, _config.ReapplyOverclock, _config.ReapplyOverclockTimer);
+        MainWindow.Applyer.Apply(SettingsService.RyzenADJline, false, SettingsService.ReapplyOverclock, SettingsService.ReapplyOverclockTimer);
     }
 
     private void Speed_Checked(object sender, RoutedEventArgs e)
@@ -167,18 +133,17 @@ public sealed partial class ПресетыPage
         Eco.IsChecked = false;
         Balance.IsChecked = false;
         Min_btn.IsChecked = false;
-        Max_btn.IsChecked = false;
-        ConfigLoad();
-        _config.PremadeMinActivated = false;
-        _config.PremadeEcoActivated = false;
-        _config.PremadeBalanceActivated = false;
-        _config.PremadeSpeedActivated = true;
-        _config.PremadeMaxActivated = false;
-        _config.RyzenADJline =
+        Max_btn.IsChecked = false; 
+        SettingsService.PremadeMinActivated = false;
+        SettingsService.PremadeEcoActivated = false;
+        SettingsService.PremadeBalanceActivated = false;
+        SettingsService.PremadeSpeedActivated = true;
+        SettingsService.PremadeMaxActivated = false;
+        SettingsService.RyzenADJline =
             " --tctl-temp=80 --stapm-limit=20000  --fast-limit=20000 --stapm-time=32 --slow-limit=20000 --slow-time=64 --vrm-current=120000 --vrmmax-current=120000 --vrmsoc-current=120000 --vrmsocmax-current=120000 --vrmgfx-current=120000 --prochot-deassertion-ramp=2";
-        ConfigSave();
+        SettingsService.SaveSettings();
         InitSave();
-        MainWindow.Applyer.Apply(_config.RyzenADJline, false, _config.ReapplyOverclock, _config.ReapplyOverclockTimer);
+        MainWindow.Applyer.Apply(SettingsService.RyzenADJline, false, SettingsService.ReapplyOverclock, SettingsService.ReapplyOverclockTimer);
     }
 
     private void Max_btn_Checked(object sender, RoutedEventArgs e)
@@ -187,18 +152,17 @@ public sealed partial class ПресетыPage
         Eco.IsChecked = false;
         Balance.IsChecked = false;
         Speed.IsChecked = false;
-        Min_btn.IsChecked = false;
-        ConfigLoad();
-        _config.PremadeMinActivated = false;
-        _config.PremadeEcoActivated = false;
-        _config.PremadeBalanceActivated = false;
-        _config.PremadeSpeedActivated = false;
-        _config.PremadeMaxActivated = true;
-        _config.RyzenADJline =
+        Min_btn.IsChecked = false; 
+        SettingsService.PremadeMinActivated = false;
+        SettingsService.PremadeEcoActivated = false;
+        SettingsService.PremadeBalanceActivated = false;
+        SettingsService.PremadeSpeedActivated = false;
+        SettingsService.PremadeMaxActivated = true;
+        SettingsService.RyzenADJline =
             " --tctl-temp=90 --stapm-limit=45000  --fast-limit=60000 --stapm-time=80 --slow-limit=60000 --slow-time=1 --vrm-current=120000 --vrmmax-current=120000 --vrmsoc-current=120000 --vrmsocmax-current=120000 --vrmgfx-current=120000 --prochot-deassertion-ramp=2";
-        ConfigSave();
+        SettingsService.SaveSettings();
         InitSave();
-        MainWindow.Applyer.Apply(_config.RyzenADJline, false, _config.ReapplyOverclock, _config.ReapplyOverclockTimer);
+        MainWindow.Applyer.Apply(SettingsService.RyzenADJline, false, SettingsService.ReapplyOverclock, SettingsService.ReapplyOverclockTimer);
     }
 
     private void Min_btn_Unchecked_1(object sender, RoutedEventArgs e)
