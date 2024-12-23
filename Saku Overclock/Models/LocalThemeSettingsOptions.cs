a2ba800a21cf54c946847dd10ec94be05b2b2d09
Erruar <erruarbrorder@gmail.com>
2024-12-23 21:11:45 +0300

    Added and fixed LocalThemeSettingsOptions. Now app themes are stored in one file with main app theme

---
+++ Saku Overclock/Models/LocalThemeSettingsOptions.cs
@@ -0,0 +1,8 @@
+﻿using Saku_Overclock.Styles;
+
+namespace Saku_Overclock.Models;
+public class LocalThemeSettingsOptions
+{
+    public string AppBackgroundRequestedTheme { get; set; } = "Default"; // Основная строка настройки темы
+    public List<ThemeClass> CustomThemes { get; set; } = []; // Массив кастомных тем
+}
