d6ab2c5908694e5ce350d8f59c7da72e2bef85a7
Erruar <erruarbrorder@gmail.com>
2024-12-23 21:13:07 +0300

    Added List<ThemeClass> DefaultThemes, new voids SaveThemeSettings and LoadThemeSettings. Removed redunant code. Now themes are stored in one file instead of two

--- Saku Overclock/Services/LocalThemeSettingsService.cs
+++ Saku Overclock/Services/LocalThemeSettingsService.cs
@@ -4,104 +4,59 @@ using Saku_Overclock.Core.Helpers;
 using Saku_Overclock.Helpers; 
 using Windows.Storage;
 using Saku_Overclock.Styles;
+using Microsoft.UI.System;
+using Saku_Overclock.Models;
 
 namespace Saku_Overclock.Services;
 
-public class LocalThemeSettingsService : ILocalSettingsService
+public class LocalThemeSettingsService : ILocalThemeSettingsService
 {
     private const string DefaultApplicationDataFolder = "Saku Overclock/Settings/Themes";
-    private const string DefaultLocalSettingsFile = "ThemeSettings.json";
-    private const string CustomLocalSettingsFile = "CustomThemeSettings.json";
+    private const string ThemeSettingsFile = "ThemeSettings.json";
 
     private readonly IFileService _fileService;
 
     private readonly string _localApplicationData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
     private readonly string _applicationDataFolder;
-    private readonly string _localsettingsFile;
+    private readonly string _themeSettingsFile;
 
-    private IDictionary<string, object> _settings;
-
-    private bool _isInitialized;
 
     public LocalThemeSettingsService(IFileService fileService)
     {
         _fileService = fileService;
 
         _applicationDataFolder = Path.Combine(_localApplicationData, DefaultApplicationDataFolder);
-        _localsettingsFile =  DefaultLocalSettingsFile;
+        _themeSettingsFile = ThemeSettingsFile;
 
-        _settings = new Dictionary<string, object>();
-    }
+    } 
 
-    private async Task InitializeAsync()
-    {
-        if (!_isInitialized)
-        {
-            _settings = await Task.Run(() => _fileService.Read<IDictionary<string, object>>(_applicationDataFolder, _localsettingsFile)) ?? new Dictionary<string, object>();
-
-            _isInitialized = true;
-        }
-    }
-
-    public async Task<T?> ReadSettingAsync<T>(string key)
-    {
-        if (RuntimeHelper.IsMSIX)
-        {
-            if (ApplicationData.Current.LocalSettings.Values.TryGetValue(key, out var obj))
-            {
-                return await Json.ToObjectAsync<T>((string)obj);
-            }
-        }
-        else
-        {
-            await InitializeAsync();
-
-            if (_settings.Any() && _settings.TryGetValue(key, out var obj))
-            {
-                return await Json.ToObjectAsync<T>((string)obj);
-            }
-        }
-
-        return default;
-    }
-
-    public async Task SaveSettingAsync<T>(string key, T value)
-    {
-        if (RuntimeHelper.IsMSIX)
-        {
-            ApplicationData.Current.LocalSettings.Values[key] = await Json.StringifyAsync(value!);
-        }
-        else
-        {
-            await InitializeAsync();
-
-            _settings[key] = await Json.StringifyAsync(value!);
-            try
-            {
-                await Task.Run(() => _fileService.Save(_applicationDataFolder, _localsettingsFile, _settings));
-            }
-            catch
-            {
-                //Can't change theme!
-            }
-        }
-    }
-    
-    public List<ThemeClass> LoadCustomThemes()
+    public LocalThemeSettingsOptions LoadThemeSettings()
     {
         try
         {
-            var themes = _fileService.Read<List<ThemeClass>>(_applicationDataFolder, CustomLocalSettingsFile);
-            if (themes != null)
-            {
-                return themes;
-            }
+            return _fileService.Read<LocalThemeSettingsOptions>(_applicationDataFolder, _themeSettingsFile)
+                ?? new LocalThemeSettingsOptions
+                {
+                    AppBackgroundRequestedTheme = "Default",
+                    CustomThemes = DefaultThemes
+                };
         }
-        catch (Exception e)
+        catch
         {
-            Console.WriteLine(e); 
+            return new LocalThemeSettingsOptions
+            {
+                AppBackgroundRequestedTheme = "Default",
+                CustomThemes = DefaultThemes
+            };
         }
-        return [
+    }
+
+    public void SaveThemeSettings(LocalThemeSettingsOptions themeSettings)
+    {
+        _fileService.Save(_applicationDataFolder, _themeSettingsFile, themeSettings);
+    } 
+
+    private static List<ThemeClass> DefaultThemes => [
         new ()
         {
             ThemeName = "Theme_Default",
@@ -157,9 +112,9 @@ public class LocalThemeSettingsService : ILocalSettingsService
             ThemeName = "Theme_Raspberry",
             ThemeLight = true,
             ThemeCustom = false,
-            ThemeOpacity = 1.0, 
-            ThemeMaskOpacity = 1.0, 
-            ThemeCustomBg = false, 
+            ThemeOpacity = 1.0,
+            ThemeMaskOpacity = 1.0,
+            ThemeCustomBg = false,
             ThemeBackground = "ms-appx:///Assets/Themes/fw41KXN.png"
         },
         new ()
@@ -183,10 +138,4 @@ public class LocalThemeSettingsService : ILocalSettingsService
             ThemeBackground = "ms-appx:///Assets/Themes/ZqjqlOs.png"
         }
     ];
-    }
-
-    public void SaveCustomThemes(List<ThemeClass> themes)
-    {
-        _fileService.Save(_applicationDataFolder, CustomLocalSettingsFile, themes);
-    }
 }
