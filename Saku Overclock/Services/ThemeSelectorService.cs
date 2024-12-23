9863bbf2881e834eecf0046f19e646032def47fc
Erruar <erruarbrorder@gmail.com>
2024-12-23 21:14:14 +0300

    Renamed ILocalThemeSettingsService, fixed ThemeBackgrounds in ThemeSelectorService. Removed old load and save files code. Code cleaning

--- Saku Overclock/Services/ThemeSelectorService.cs
+++ Saku Overclock/Services/ThemeSelectorService.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.UI.Xaml;
 using Saku_Overclock.Contracts.Services;
-using Saku_Overclock.Core.Contracts.Services;
 using Saku_Overclock.Helpers;
 using Saku_Overclock.Styles;
 
@@ -8,15 +7,13 @@ namespace Saku_Overclock.Services;
 
 public class ThemeSelectorService : IThemeSelectorService
 {
-    private const string SettingsKey = "AppBackgroundRequestedTheme";
-
     public ElementTheme Theme
     {
         get;
         set;
     } = ElementTheme.Default;
 
-    private readonly ILocalSettingsService _localThemeSettingsService;
+    private readonly ILocalThemeSettingsService _localThemeSettingsService;
 
     public List<ThemeClass> Themes
     {
@@ -24,98 +21,96 @@ public class ThemeSelectorService : IThemeSelectorService
         set;
     }
 
-    public ThemeSelectorService(ILocalSettingsService localThemeSettingsService)
+    public ThemeSelectorService(ILocalThemeSettingsService localThemeSettingsService)
     {
         _localThemeSettingsService = localThemeSettingsService;
-        Themes =
-        [
-            new()
-            {
-                ThemeName = "Theme_Default",
-                ThemeLight = false,
-                ThemeCustom = false,
-                ThemeOpacity = 0.0,
-                ThemeMaskOpacity = 0.0,
-                ThemeCustomBg = false,
-                ThemeBackground = ""
-            },
-            new()
-            {
-                ThemeName = "Theme_Light",
-                ThemeLight = true,
-                ThemeCustom = false,
-                ThemeOpacity = 0.0,
-                ThemeMaskOpacity = 0.0,
-                ThemeCustomBg = false,
-                ThemeBackground = ""
-            },
-            new()
-            {
-                ThemeName = "Theme_Dark",
-                ThemeLight = false,
-                ThemeCustom = false,
-                ThemeOpacity = 0.0,
-                ThemeMaskOpacity = 0.0,
-                ThemeCustomBg = false,
-                ThemeBackground = ""
-            },
-            new()
-            {
-                ThemeName = "Theme_Clouds",
-                ThemeLight = true,
-                ThemeCustom = false,
-                ThemeOpacity = 0.5,
-                ThemeMaskOpacity = 1.0,
-                ThemeCustomBg = false,
-                ThemeBackground = "https://i.imgur.com/DuwlKmK.png"
-            },
-            new()
-            {
-                ThemeName = "Theme_Neon",
-                ThemeLight = false,
-                ThemeCustom = false,
-                ThemeOpacity = 0.3,
-                ThemeMaskOpacity = 0.3,
-                ThemeCustomBg = false,
-                ThemeBackground = "https://i.imgur.com/DuwlKmK.png"
-            },
-            new()
-            {
-                ThemeName = "Theme_Raspberry",
-                ThemeLight = true,
-                ThemeCustom = false,
-                ThemeOpacity = 1.0,
-                ThemeMaskOpacity = 1.0,
-                ThemeCustomBg = false,
-                ThemeBackground = "https://i.imgur.com/fw41KXN.png"
-            },
-            new()
-            {
-                ThemeName = "Theme_Sand",
-                ThemeLight = true,
-                ThemeCustom = false,
-                ThemeOpacity = 0.7,
-                ThemeMaskOpacity = 1.0,
-                ThemeCustomBg = false,
-                ThemeBackground = "https://i.imgur.com/ZqjqlOs.png"
-            },
-            new()
-            {
-                ThemeName = "Theme_Coffee",
-                ThemeLight = false,
-                ThemeCustom = false,
-                ThemeOpacity = 0.3,
-                ThemeMaskOpacity = 1.0,
-                ThemeCustomBg = false,
-                ThemeBackground = "https://i.imgur.com/ZqjqlOs.png"
-            }
-        ];
+        Themes = [
+        new ()
+        {
+            ThemeName = "Theme_Default",
+            ThemeLight = false,
+            ThemeCustom = false,
+            ThemeOpacity = 0.0,
+            ThemeMaskOpacity = 0.0,
+            ThemeCustomBg = false,
+            ThemeBackground = ""
+        },
+        new ()
+        {
+            ThemeName = "Theme_Light",
+            ThemeLight = true,
+            ThemeCustom = false,
+            ThemeOpacity = 0.0,
+            ThemeMaskOpacity = 0.0,
+            ThemeCustomBg = false,
+            ThemeBackground = ""
+        },
+        new ()
+        {
+            ThemeName = "Theme_Dark",
+            ThemeLight = false,
+            ThemeCustom = false,
+            ThemeOpacity = 0.0,
+            ThemeMaskOpacity = 0.0,
+            ThemeCustomBg = false,
+            ThemeBackground = ""
+        },
+        new ()
+        {
+            ThemeName = "Theme_Clouds",
+            ThemeLight = true,
+            ThemeCustom = false,
+            ThemeOpacity = 0.5,
+            ThemeMaskOpacity = 1.0,
+            ThemeCustomBg = false,
+            ThemeBackground = "ms-appx:///Assets/Themes/DuwlKmK.png"
+        },
+        new ()
+        {
+            ThemeName = "Theme_Neon",
+            ThemeLight = false,
+            ThemeCustom = false,
+            ThemeOpacity = 0.3,
+            ThemeMaskOpacity = 0.3,
+            ThemeCustomBg = false,
+            ThemeBackground = "ms-appx:///Assets/Themes/DuwlKmK.png"
+        },
+        new ()
+        {
+            ThemeName = "Theme_Raspberry",
+            ThemeLight = true,
+            ThemeCustom = false,
+            ThemeOpacity = 1.0,
+            ThemeMaskOpacity = 1.0,
+            ThemeCustomBg = false,
+            ThemeBackground = "ms-appx:///Assets/Themes/fw41KXN.png"
+        },
+        new ()
+        {
+            ThemeName = "Theme_Sand",
+            ThemeLight = true,
+            ThemeCustom = false,
+            ThemeOpacity = 0.7,
+            ThemeMaskOpacity = 1.0,
+            ThemeCustomBg = false,
+            ThemeBackground = "ms-appx:///Assets/Themes/ZqjqlOs.png"
+        },
+        new ()
+        {
+            ThemeName = "Theme_Coffee",
+            ThemeLight = false,
+            ThemeCustom = false,
+            ThemeOpacity = 0.3,
+            ThemeMaskOpacity = 1.0,
+            ThemeCustomBg = false,
+            ThemeBackground = "ms-appx:///Assets/Themes/ZqjqlOs.png"
+        }
+    ];
     }
 
     public async Task InitializeAsync()
     {
-        Theme = await LoadThemeFromSettingsAsync();
-        LoadCustomTheme();
+        LoadThemeFromSettings();
         await Task.CompletedTask;
     }
 
@@ -124,7 +119,7 @@ public class ThemeSelectorService : IThemeSelectorService
         Theme = theme;
 
         await SetRequestedThemeAsync();
-        await SaveThemeInSettingsAsync(Theme);
+        SaveThemeInSettings();
     }
 
     public async Task SetRequestedThemeAsync()
@@ -139,35 +134,25 @@ public class ThemeSelectorService : IThemeSelectorService
         await Task.CompletedTask;
     }
     
-    public void LoadCustomTheme()
+    public void LoadThemeFromSettings()
     {
-        Themes = _localThemeSettingsService.LoadCustomThemes();
-    }
-
-    public void SaveCustomTheme()
-    {
-        _localThemeSettingsService.SaveCustomThemes(Themes);
-    }
-
-    private async Task<ElementTheme> LoadThemeFromSettingsAsync()
-    {
+        Themes = _localThemeSettingsService.LoadThemeSettings().CustomThemes;
         try
         {
-            var themeName = await _localThemeSettingsService.ReadSettingAsync<string>(SettingsKey);
-            if (Enum.TryParse(themeName, out ElementTheme cacheTheme))
+            if (Enum.TryParse(_localThemeSettingsService.LoadThemeSettings().AppBackgroundRequestedTheme, out ElementTheme cacheTheme))
             {
-                return cacheTheme;
+                Theme = cacheTheme;
             }
         }
         catch
         {
-            return ElementTheme.Default;
+            Theme = ElementTheme.Default;
         }
-        return ElementTheme.Default;
-    } 
-
-    private async Task SaveThemeInSettingsAsync(ElementTheme theme)
-    {
-        await _localThemeSettingsService.SaveSettingAsync(SettingsKey, theme.ToString());
+        
     }
+
+    public void SaveThemeInSettings()
+    {
+        _localThemeSettingsService.SaveThemeSettings(new Models.LocalThemeSettingsOptions() { CustomThemes = Themes, AppBackgroundRequestedTheme = Theme.ToString()});
+    } 
 }
