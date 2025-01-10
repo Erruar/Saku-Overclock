f53342a06410f3b2c1e2467fd9dad59838a6a4b1
Erruar <erruarbrorder@gmail.com>
2025-01-10 20:01:00 +0300

    Added Training Page ViewModel

---
+++ Saku Overclock/ViewModels/ОбучениеViewModel.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Saku_Overclock.ViewModels;
+
+public partial class ОбучениеViewModel : ObservableRecipient
+{
+    public ОбучениеViewModel() { }
+}
