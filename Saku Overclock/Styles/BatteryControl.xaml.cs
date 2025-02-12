a050112c4e8936591e2a7629a44b9b0401c96eae
Erruar <erruarbrorder@gmail.com>
2025-02-12 15:39:40 +0300

    BatteryControl: Added custom element showing the battery and its current state

---
+++ Saku Overclock/Styles/BatteryControl.xaml.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Saku_Overclock.Styles;
+public sealed partial class BatteryControl : UserControl
+{
+    private string? _value;
+    public string? Value 
+    {
+        get => _value;
+        set => SetBattery(value);
+    }
+    private void SetBattery(string? value)
+    {
+        _value = value;
+        BatteryText.Text = value != null && value.Contains('%') ? value : value + "%";
+        var dimSize = BatteryPercentGrid.ActualWidth * Convert.ToInt32(value) / 100;
+        if (dimSize != 0)
+        {
+            BatteryPercentBorder.Width = dimSize;
+        }
+        else
+        {
+            BatteryPercentBorder.Width = BatteryPercentGrid.ActualWidth;
+        }
+    }
+    public BatteryControl()
+    {
+        InitializeComponent();
+        Loaded += BatteryControl_Loaded;
+        SizeChanged += BatteryControl_SizeChanged;
+    }
+
+    private void BatteryControl_SizeChanged(object sender, SizeChangedEventArgs e)
+    {
+        SetBattery(Value);
+    }
+    private void BatteryControl_Loaded(object sender, RoutedEventArgs e)
+    {
+        SetBattery(Value); 
+    }
+}
