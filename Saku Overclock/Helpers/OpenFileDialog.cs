c7a8f022130b7f7c4383527890836a522ad8cb2c
Erruar <erruarbrorder@gmail.com>
2025-02-12 15:39:00 +0300

    OpemFileDialog: a class that uses DCOM Windows events, returning a File dialog to select the background of a custom theme in .NET 9. Since there are no other ways to invoke the file dialog, it was decided to use this implementation. (WIP)

---
+++ Saku Overclock/Helpers/OpenFileDialog.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.InteropServices; 
+
+namespace Saku_Overclock.Helpers;
+public static class OpenFileDialog
+{
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+    public class OpenFileName
+    {
+        public int lStructSize = Marshal.SizeOf(typeof(OpenFileName));
+        public IntPtr hwndOwner;
+        public IntPtr hInstance;
+        public string lpstrFilter;
+        public string lpstrCustomFilter;
+        public int nMaxCustFilter;
+        public int nFilterIndex;
+        public string lpstrFile;
+        public int nMaxFile;
+        public string lpstrFileTitle;
+        public int nMaxFileTitle;
+        public string lpstrInitialDir;
+        public string lpstrTitle;
+        public int Flags;
+        public short nFileOffset;
+        public short nFileExtension;
+        public string lpstrDefExt;
+        public IntPtr lCustData;
+        public IntPtr lpfnHook;
+        public string lpTemplateName;
+        public IntPtr pvReserved;
+        public int dwReserved;
+        public int flagsEx;
+    }
+
+    [DllImport("Comdlg32.dll", SetLastError = true, ThrowOnUnmappableChar = true, CharSet = CharSet.Auto)]
+    public static extern bool GetOpenFileName([In, Out] OpenFileName ofn);
+}
