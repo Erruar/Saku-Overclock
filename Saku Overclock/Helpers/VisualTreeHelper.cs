fa81a033cdf2ac2cb93c246e74bcf9229ec4f93a
Erruar <erruarbrorder@gmail.com>
2025-02-12 15:37:35 +0300

    VisualTreeHelper: a class that helps to find UI elements in the main project backend code flow, which helps to modify and manage UI elements directly in real time, making it possible to search for parameters, improve properties of standard UI objects (e.g. add shadows or change location offset) and find the right element on the page.

---
+++ Saku Overclock/Helpers/VisualTreeHelper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace Saku_Overclock.Helpers;
+
+internal static class VisualTreeHelper
+{
+    public static IEnumerable<T> FindVisualChildren<T>(DependencyObject parent) where T : DependencyObject
+    {
+        for (var i = 0; i < Microsoft.UI.Xaml.Media.VisualTreeHelper.GetChildrenCount(parent); i++)
+        {
+            var child = Microsoft.UI.Xaml.Media.VisualTreeHelper.GetChild(parent, i);
+            if (child is T t)
+            {
+                yield return t;
+            }
+
+            foreach (var childOfChild in FindVisualChildren<T>(child))
+            {
+                yield return childOfChild;
+            }
+        }
+    }
+
+    public static Grid? FindAdjacentGrid(StackPanel stackPanel)
+    {
+        var parent = Microsoft.UI.Xaml.Media.VisualTreeHelper.GetParent(stackPanel) as Panel;
+        if (parent == null)
+        {
+            return null;
+        }
+
+        var index = parent.Children.IndexOf(stackPanel);
+        return index < 0 || index >= parent.Children.Count - 1 ? null : parent.Children[index + 1] as Grid;
+    }
+
+    public static bool FindAjantedFontIcons(FontIcon fontIcon, List<string> glyphs)
+    {
+        foreach (var glyph in glyphs)
+        {
+            if (fontIcon.Glyph.Contains(glyph))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void SetAllChildrenVisibility(StackPanel parent, Visibility visibility)
+    {
+        var stackPanels = FindVisualChildren<StackPanel>(parent);
+        foreach (var stackPanel in stackPanels)
+        {
+            stackPanel.Visibility = visibility;
+        }
+    }
+
+}
