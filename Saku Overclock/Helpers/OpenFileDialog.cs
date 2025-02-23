e8980aadd2f3989c64c64bedd9533188bb6abe35
Erruar <erruarbrorder@gmail.com>
2025-02-23 19:27:51 +0300

    OpenFileDialog Backend: changed struct layout from class to struct, added MarshalAs attributes for string properties, and updated DllImport.

--- Saku Overclock/Helpers/OpenFileDialog.cs
+++ Saku Overclock/Helpers/OpenFileDialog.cs
@@ -1,36 +1,62 @@
-﻿using System.Runtime.InteropServices; 
+﻿using System.Runtime.InteropServices;
+using System.Text;
 
 namespace Saku_Overclock.Helpers;
+
+[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+public struct OpenFileName
+{
+    public int lStructSize;
+    public IntPtr hwndOwner;
+    public IntPtr hInstance;
+
+    [MarshalAs(UnmanagedType.LPTStr)]
+    public string lpstrFilter;
+
+    [MarshalAs(UnmanagedType.LPTStr)]
+    public string lpstrCustomFilter;
+
+    public int nMaxCustFilter;
+    public int nFilterIndex;
+
+    // Поле для пути к файлу: фиксированная длина 256 символов
+    [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+    public string lpstrFile;
+
+    public int nMaxFile;
+
+    // Если необходимо получать только имя файла (без пути)
+    [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+    public string lpstrFileTitle;
+
+    public int nMaxFileTitle;
+
+    [MarshalAs(UnmanagedType.LPTStr)]
+    public string lpstrInitialDir;
+
+    [MarshalAs(UnmanagedType.LPTStr)]
+    public string lpstrTitle;
+
+    public int Flags;
+    public short nFileOffset;
+    public short nFileExtension;
+
+    [MarshalAs(UnmanagedType.LPTStr)]
+    public string lpstrDefExt;
+
+    public IntPtr lCustData;
+    public IntPtr lpfnHook;
+
+    [MarshalAs(UnmanagedType.LPTStr)]
+    public string lpTemplateName;
+
+    public IntPtr pvReserved;
+    public int dwReserved;
+    public int flagsEx;
+}
 public static class OpenFileDialog
 {
-    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
-    public class OpenFileName
-    {
-        public int lStructSize = Marshal.SizeOf(typeof(OpenFileName));
-        public IntPtr hwndOwner;
-        public IntPtr hInstance;
-        public string lpstrFilter;
-        public string lpstrCustomFilter;
-        public int nMaxCustFilter;
-        public int nFilterIndex;
-        public string lpstrFile;
-        public int nMaxFile;
-        public string lpstrFileTitle;
-        public int nMaxFileTitle;
-        public string lpstrInitialDir;
-        public string lpstrTitle;
-        public int Flags;
-        public short nFileOffset;
-        public short nFileExtension;
-        public string lpstrDefExt;
-        public IntPtr lCustData;
-        public IntPtr lpfnHook;
-        public string lpTemplateName;
-        public IntPtr pvReserved;
-        public int dwReserved;
-        public int flagsEx;
-    }
-
-    [DllImport("Comdlg32.dll", SetLastError = true, ThrowOnUnmappableChar = true, CharSet = CharSet.Auto)]
-    public static extern bool GetOpenFileName([In, Out] OpenFileName ofn);
+    [DllImport("Comdlg32.dll", SetLastError = true,
+        ThrowOnUnmappableChar = true, CharSet = CharSet.Auto)]
+    public static extern bool GetOpenFileName(ref OpenFileName ofn);
 }
