f7ba935c978166f40669dee0b0d848e3a5f90ec3
Erruar <erruarbrorder@gmail.com>
2025-04-24 22:10:52 +0300

    Fresh app build with VS and new animation file

--- Saku Overclock.Core/obj/Debug/net7.0/Saku Overclock.Core.AssemblyInfo.cs
+++ Saku Overclock.Core/obj/Debug/net7.0/Saku Overclock.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Saku Overclock.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.1.5")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+caa8071f1ca741fb7fc64e1b390464f28f6d767c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b998a63f4f4a6590a9cf04af362085d2a9628323")]
 [assembly: System.Reflection.AssemblyProductAttribute("Saku Overclock.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Saku Overclock.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.1.5")]
