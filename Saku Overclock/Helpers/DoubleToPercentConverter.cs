0536eaf450b413827f64fdd67797d686e8b2c90a
Erruar <erruarbrorder@gmail.com>
2025-07-18 20:27:45 +0300

    [Feat] Added Double to percent and Round helper converters

---
+++ Saku Overclock/Helpers/DoubleToPercentConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml.Data;
+
+namespace Saku_Overclock.Helpers;
+
+public partial class DoubleToPercentConverter : IValueConverter
+{
+    public object? Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value is double doubleValue)
+        {
+            if ((doubleValue - 50) > 0)
+            {
+                return $"+{doubleValue - 50}%";
+            }
+            return $"{doubleValue - 50}%";
+        }
+        return null;
+    }
+
+    public object? ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        return null;
+    }
+}
---
+++ Saku Overclock/Helpers/RoundConvertersHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.UI.Xaml.Data;
+
+namespace Saku_Overclock.Helpers;
+
+public partial class DoubleRound2Converter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string input) => Math.Round((double)value, 2);
+    public object ConvertBack(object value, Type targetType, object parameter, string input) => new NotImplementedException();
+}
+
+public partial class DoubleRound3Converter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string input) => Math.Round((double)value, 3);
+    public object ConvertBack(object value, Type targetType, object parameter, string input) => new NotImplementedException();
+}
