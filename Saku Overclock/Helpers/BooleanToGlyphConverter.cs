4e45bf491a0b14258c56472e21cf0b901d37aa93
Erruar <erruarbrorder@gmail.com>
2025-07-18 20:30:23 +0300

    [Fix] Fixed Boolean to glyph converter

--- Saku Overclock/Helpers/BooleanToGlyphConverter.cs
+++ Saku Overclock/Helpers/BooleanToGlyphConverter.cs
@@ -7,7 +7,7 @@ using Microsoft.UI.Xaml.Data;
 
 namespace Saku_Overclock.Helpers;
 
-public class BooleanToGlyphConverter : IValueConverter
+public partial class BooleanToGlyphConverter : IValueConverter
 {
     public object? Convert(object value, Type targetType, object parameter, string language)
     {
