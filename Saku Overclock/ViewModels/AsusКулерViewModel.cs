124fee9bd82d50d4ef4989b6d41975339593b339
Erruar <erruarbrorder@gmail.com>
2025-12-21 18:59:55 +0300

    [Remove] Removed Asus cooler page viewmodel

--- Saku Overclock/ViewModels/AsusКулерViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace Saku_Overclock.ViewModels;
-public partial class AsusКулерViewModel : ObservableRecipient
-{
-    public AsusКулерViewModel()
-    {
-    }
-} 
