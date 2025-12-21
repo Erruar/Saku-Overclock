c984c05099e09a131c16cad7156d0d44da1a9671
Erruar <erruarbrorder@gmail.com>
2025-12-21 21:28:31 +0300

    [Add] Added UpdateAppliedTheme to Theme Selector Service

--- Saku Overclock/Services/ThemeSelectorService.cs
+++ Saku Overclock/Services/ThemeSelectorService.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Imaging;
 using Saku_Overclock.Contracts.Services;
 using Saku_Overclock.Helpers;
 using Saku_Overclock.Models;
@@ -20,10 +22,9 @@ public class ThemeSelectorService(ILocalThemeSettingsService localThemeSettingsS
         set;
     } = localThemeSettingsService.GetDefaultThemes();
 
-    public async Task InitializeAsync()
+    public void Initialize()
     {
         LoadThemeFromSettings();
-        await Task.CompletedTask;
     }
 
     public async Task SetThemeAsync(ElementTheme theme)
@@ -46,6 +47,79 @@ public class ThemeSelectorService(ILocalThemeSettingsService localThemeSettingsS
         await Task.CompletedTask;
     }
 
+    public sealed class ThemeApplyResult
+    {
+        public ImageSource? BackgroundImageSource
+        {
+            get; init;
+        }
+        public double ThemeOpacity
+        {
+            get; init;
+        }
+        public double ThemeMaskOpacity
+        {
+            get; init;
+        }
+        public string? ThemeApplyMessage
+        {
+            get; init; 
+        }
+    }
+
+    public async Task<ThemeApplyResult> UpdateAppliedTheme(int themeType)
+    {
+        const int BuiltInThemesCount = 2;
+
+        ImageSource? imageSource = null;
+        double themeOpacity = 1;
+        double themeMaskOpacity = 1;
+        var message = string.Empty;
+
+        if (themeType < Themes.Count && themeType > -1)
+        {
+            var themeLight = Themes[themeType].ThemeLight
+                    ? ElementTheme.Light
+                    : ElementTheme.Dark;
+            await SetThemeAsync(themeType == 0 ? ElementTheme.Default : themeLight);
+            if (Themes[themeType].ThemeCustomBg ||
+                Themes[themeType].ThemeName.Contains("Theme_"))
+            {
+                var themeBackground = Themes[themeType].ThemeBackground;
+
+                if (themeType > BuiltInThemesCount &&
+                    !string.IsNullOrEmpty(themeBackground) &&
+                    (themeBackground.Contains("http") || themeBackground.Contains("appx") ||
+                     File.Exists(themeBackground)))
+                {
+                    try
+                    {
+                        imageSource = new BitmapImage(new Uri(themeBackground));
+                    }
+                    catch
+                    {
+                        message = "ThemeNotFoundBg".GetLocalized();
+                    }
+                }
+            }
+            
+            themeOpacity = Themes[themeType].ThemeOpacity;
+            themeMaskOpacity = Themes[themeType].ThemeMaskOpacity;
+        }
+        else
+        {
+            message = "ThemeNotFound".GetLocalized();
+        }
+
+        return new ThemeApplyResult()
+        {
+            BackgroundImageSource = imageSource,
+            ThemeOpacity = themeOpacity,
+            ThemeMaskOpacity = themeMaskOpacity,
+            ThemeApplyMessage = message
+        };
+    }
+
     public void LoadThemeFromSettings()
     {
         var retValue = localThemeSettingsService.LoadThemeSettings();
@@ -55,7 +129,7 @@ public class ThemeSelectorService(ILocalThemeSettingsService localThemeSettingsS
 
             try
             {
-                if (Enum.TryParse(localThemeSettingsService.LoadThemeSettings().AppBackgroundRequestedTheme,
+                if (Enum.TryParse(retValue.AppBackgroundRequestedTheme,
                         out ElementTheme cacheTheme))
                 {
                     Theme = cacheTheme;
